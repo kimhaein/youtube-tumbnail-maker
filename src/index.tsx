55f298e530f2c1fe85b4828fb4c3924c7af42f59
haein <kendy93@sbcn.co.kr>
2019-07-13 18:32:22 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./static/style.sass";
+import "./static/style.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
