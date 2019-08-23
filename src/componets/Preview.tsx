4bcf5563a9e9f14740cdb97f7f74ea2bd4dd04ed
haein <kendy93@sbcn.co.kr>
2019-08-23 16:44:23 +0900

    스타일 수정

--- src/componets/Preview.tsx
+++ src/componets/Preview.tsx
@@ -10,6 +10,7 @@ export const Preview: React.FC<PreviewProps> = props => {
     <div className="perview">
       <canvas ref={props.canvasRef} onClick={props.getPosition} />
       <p>* 해당 영역을 클릭하면 글자의 위치를 지정할 수 있습니다.</p>
+      <p>* 해상도를 유지하기 위해 입력하신 크기에 2배 이미지로 다운로드 됩니다.</p>
     </div>
   );
 };
